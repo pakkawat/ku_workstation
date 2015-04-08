666b55d01c018160b0882798f0132d500037c2e8
pakkawat <pakkawat.jong@gmail.com>
2015-04-08 19:17:55 +0700

    UserSubjects add update and button add
    UserSubjects add update and button add

--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -4,4 +4,12 @@ class UserSubjectsController < ApplicationController
     @subjectusers = @subject.ku_users
     @users = KuUser.where.not(id: @subjectusers)
   end
+
+  def update
+    @kuuser = KuUser.find(params[:ku_user_id])
+    @subject = Subject.find(params[:subject_id])
+    @subject.usersubjects.create(kuuser: @kuuser)
+    redirect_to subject_user_subjects_path(:subject_id => @subject.id)
+  end
+
 end
