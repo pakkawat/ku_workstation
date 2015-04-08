a242e6f92e7d59242622f35f0a40f838790c7104
pakkawat <pakkawat.jong@gmail.com>
2015-04-08 18:10:22 +0700

    UserSubjects
    UserSubjects

--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -1,5 +1,7 @@
 class UserSubjectsController < ApplicationController
   def index
     @subject = Subject.find(params[:id])  
+    @subjectusers = @subject.ku_users
+    @users = KuUser.Subject.where.not(subject_id: @subject)
   end
 end
