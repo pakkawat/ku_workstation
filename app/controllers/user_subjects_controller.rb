dd104c4a0e14b9830af14576de977d3df5991ea6
pakkawat <pakkawat.jong@gmail.com>
2015-04-09 18:16:32 +0700

    UserSubject partial user list
    UserSubject partial user list

--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -2,7 +2,7 @@ class UserSubjectsController < ApplicationController
   def index
     @subject = Subject.find(params[:subject_id])  
     @subjectusers = @subject.ku_users
-    @users = KuUser.where.not(id: @subjectusers)
+    @kuusers = KuUser.where.not(id: @subjectusers)
   end
 
   def create
