b50450c621526258a697db1cfeed3af5c6451d33
Ubuntu <ubuntu@ip-172-31-21-54.ap-southeast-1.compute.internal>
2015-04-09 09:03:53 +0000

    UserSubject fix delete button

--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -12,17 +12,10 @@ class UserSubjectsController < ApplicationController
     redirect_to subject_user_subjects_path(:subject_id => @subject.id)
   end
 
-  def update
+  def destroy
     @kuuser = KuUser.find(params[:ku_user_id])
     @subject = Subject.find(params[:subject_id])
-    @subject.usersubjects.create(kuuser: @kuuser)
-    redirect_to subject_user_subjects_path(:subject_id => @subject.id)
-  end
-
-  def destroy
-  	@kuuser = KuUser.find(params[:ku_user_id])
-    @subject = Subject.find(params[:subject_id])
-    @subject.usersubjects.destroy(kuuser: @kuuser)
+    UserSubject.find_by(ku_user_id: @kuuser.id, subject_id: @subject.id).destroy
     redirect_to subject_user_subjects_path(:subject_id => @subject.id)
   end
 end
