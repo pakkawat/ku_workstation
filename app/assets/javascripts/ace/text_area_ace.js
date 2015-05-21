25a223d8e3b776424c8b46cb163ffb58ec8e2081
pakkawat <pakkawat.jong@gmail.com>
2015-05-21 14:20:29 +0700

    fix ace editor and disable syntax check
    fix ace editor and disable syntax check

--- app/assets/javascripts/ace/text_area_ace.js
+++ app/assets/javascripts/ace/text_area_ace.js
@@ -1,8 +1,9 @@
-var textarea = $('#content');
+var textarea = $('#file_data');
 
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.getSession().setMode("ace/mode/javascript");
+editor.getSession().setUseWorker(false);
 editor.setOptions({
     maxLines: Infinity
 });
