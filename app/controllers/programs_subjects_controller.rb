f971418a1584185ad00ca7a8d72a675ce79e055e
pakkawat <pakkawat.jong@gmail.com>
2015-06-05 13:12:37 +0700

    update programs_subjects and user_subjects
    update programs_subjects and user_subjects

--- app/controllers/programs_subjects_controller.rb
+++ app/controllers/programs_subjects_controller.rb
@@ -1,45 +1,80 @@
 class ProgramsSubjectsController < ApplicationController
   def index
     @subject = Subject.find(params[:subject_id])  
-    @subjectprograms = @subject.programs.paginate(page: params[:subjectprogram_page], per_page: 2).order("program_name ASC")
-    @programs = Program.where.not(id: @subject.programs).paginate(page: params[:program_page], per_page: 2).order("program_name ASC")
+    @subjectprograms = Program.where(id: @subject.programs_subjects.select("program_id").where(program_enabled: true)).paginate(page: params[:subjectprogram_page], per_page: 2).order("program_name ASC")
+    @programs = Program.where.not(id: @subjectprograms).paginate(page: params[:program_page], per_page: 2).order("program_name ASC")
   end
 
   def create
     @program = Program.find(params[:program_id])
     @subject = Subject.find(params[:subject_id])
-    @subject.programs_subjects.create(program: @program)
-    add_program_to_run_list
+    #@subject.programs_subjects.create(program: @program)
+    #add_program_to_run_list
+    #redirect_to subject_programs_subjects_path(:subject_id => @subject.id)
+
+
+    @program_enabled = @subject.programs_subjects.find_by(program_id: @program.id)
+    if @program_enabled.present?
+      if @program_enabled.update_attribute(:program_enabled, true)
+        update_program_to_run_list
+        flash[:success] = @program.program_name + " has been added"
+      else
+        flash[:danger] = "Error1!!"
+      end
+    else
+      if @subject.programs_subjects.save(program: @program)
+        add_program_to_run_list
+        flash[:success] = @program.program_name + " has been added"
+      else
+        flash[:danger] = "Error2!!"
+      end
+    end#@program_enabled.present?
     redirect_to subject_programs_subjects_path(:subject_id => @subject.id)
   end
 
   def destroy
     @program = Program.find(params[:program_id])
     @subject = Subject.find(params[:subject_id])
-    ProgramsSubject.find_by(program_id: @program.id, subject_id: @subject.id).destroy
-    add_remove_program_to_run_list
+    #ProgramsSubject.find_by(program_id: @program.id, subject_id: @subject.id).destroy
+    #add_remove_program_to_run_list
+    #redirect_to subject_programs_subjects_path(:subject_id => @subject.id)
+
+    if @subject.programs_subjects.find_by(program_id: @program.id).update_attribute(:program_enabled, false)
+      add_remove_program_to_run_list
+      flash[:success] = @program.program_name + " has been deleted from subject"
+    else
+      flash[:danger] = "Error3!!"
+    end
     redirect_to subject_programs_subjects_path(:subject_id => @subject.id)
   end
 
-  private
-    def program_apply# send run_list to Chef-server and run sudo chef-clients then if any remove need update user.run_list
-      str_temp = ""
-      @subject = Subject.find(params[:subject_id])
-      @subject.ku_users.each do |user|
-        str_temp += "ku_id: " + user.ku_id + " - run_list:" + user.run_list.gsub(/\,$/, '')
-        str_temp += " || "
-        user.update_column(:run_list, @kuuser.run_list.gsub("recipe[remove-" + program.program_name + "],", ""))
-        # delete recipe[remove-xxx], from user.run_list
-      end
-
-      render plain: str_temp.inspect
+  def program_apply# send run_list to Chef-server and run sudo chef-clients then if any remove need update user.run_list
+    str_temp = ""
+    @subject = Subject.find(params[:subject_id])
+    @subject.ku_users.each do |user|# send run_list to Chef-server and run sudo chef-clients 
+      str_temp += "ku_id: " + user.ku_id + " - run_list:" + user.run_list.gsub(/\,$/, '')
+      str_temp += " || "
     end
 
+    Program.where(id: @subject.programs_subjects.select("program_id").where(program_enabled: false)).each do |program|
+      @subject.ku_users.each do |user|
+        # delete recipe[remove-xxx], from user.run_list
+        user.update_column(:run_list, user.run_list.gsub("recipe[remove-" + program.program_name + "],", ""))
+      end
+    end
+    # delete relationship
+    @subject.programs_subjects.where(program_enabled: false).destroy_all
+    flash[:success] = str_temp
+    redirect_to subject_programs_subjects_path(:subject_id => @subject.id)
+
+  end
+
+  private
     def add_program_to_run_list
       str_temp = ""
-      @program = Program.find(params[:program_id])
-      @subject = Subject.find(params[:subject_id])
-      @subject.ku_users.each do |user|
+      #@program = Program.find(params[:program_id])
+      #@subject = Subject.find(params[:subject_id])
+      KuUser.where(id: @subject.user_subjects.select("ku_user_id").where(user_enabled: true)).each do |user|
         #str_temp += "ku_id: " + user.ku_id + " add recipe[" + @program.program_name + "] || "
         user.update_column(:run_list, user.run_list + "recipe[" + @program.program_name + "],")
       end
@@ -48,10 +83,17 @@ class ProgramsSubjectsController < ApplicationController
 
     def add_remove_program_to_run_list
       str_temp = ""
-      @program = Program.find(params[:program_id])
-      @subject = Subject.find(params[:subject_id])
-      @subject.ku_users.each do |user|
+      #@program = Program.find(params[:program_id])
+      #@subject = Subject.find(params[:subject_id])
+      KuUser.where(id: @subject.user_subjects.select("ku_user_id").where(user_enabled: true)).each do |user|
         user.update_column(:run_list, user.run_list.gsub("recipe[" + @program.program_name + "],", "recipe[remove-" + @program.program_name + "],"))
+      end
+    end
+
+    def update_program_to_run_list
+      str_temp = ""
+      KuUser.where(id: @subject.user_subjects.select("ku_user_id").where(user_enabled: true)).each do |user|
+        user.update_column(:run_list, user.run_list.gsub("recipe[remove-" + @program.program_name + "],", "recipe[" + @program.program_name + "],"))
       end
     end
 end
--- app/controllers/user_subjects_controller.rb
+++ app/controllers/user_subjects_controller.rb
@@ -1,45 +1,71 @@
 class UserSubjectsController < ApplicationController
   def index
     @subject = Subject.find(params[:subject_id])  
-    @subjectusers = @subject.ku_users.paginate(page: params[:subjectuser_page], per_page: 2).order("ku_id ASC")
-    @kuusers = KuUser.where.not(id: @subject.ku_users).paginate(page: params[:kuuser_page], per_page: 2).order("ku_id ASC")
+    @subjectusers = KuUser.where(id: @subject.user_subjects.select("ku_user_id").where(user_enabled: true)).paginate(page: params[:subjectuser_page], per_page: 2).order("ku_id ASC")
+    @kuusers = KuUser.where.not(id: @subjectusers).paginate(page: params[:kuuser_page], per_page: 2).order("ku_id ASC")
   end
 
   def create
     @kuuser = KuUser.find(params[:ku_user_id])
     @subject = Subject.find(params[:subject_id])
-    @subject.user_subjects.create(ku_user: @kuuser)
-    add_program_to_run_list
+    @user_enabled = @subject.user_subjects.find_by(ku_user_id: @kuuser.id)
+    if @user_enabled.present?
+      if @user_enabled.update_attribute(:user_enabled, true)
+        update_program_to_run_list
+        flash[:success] = @kuuser.ku_id + " has been added"
+      else
+        flash[:danger] = "Error1!!"
+      end
+    else
+      if @subject.user_subjects.save(ku_user: @kuuser)
+        add_program_to_run_list
+        flash[:success] = @kuuser.ku_id + " has been added"
+      else
+        flash[:danger] = "Error2!!"
+      end
+    end#@user_enabled.present?
     redirect_to subject_user_subjects_path(:subject_id => @subject.id)
   end
 
   def destroy
     @kuuser = KuUser.find(params[:ku_user_id])
     @subject = Subject.find(params[:subject_id])
-    UserSubject.find_by(ku_user_id: @kuuser.id, subject_id: @subject.id).destroy
-    add_remove_program_to_run_list
+    #UserSubject.find_by(ku_user_id: @kuuser.id, subject_id: @subject.id).destroy
+    if @subject.user_subjects.find_by(ku_user_id: @kuuser.id).update_attribute(:user_enabled, false)
+      add_remove_program_to_run_list
+      flash[:success] = @kuuser.ku_id + " has been deleted from subject"
+    else
+      flash[:danger] = "Error3!!"
+    end
     redirect_to subject_user_subjects_path(:subject_id => @subject.id)
   end
 
-  private
-    def subject_apply# send run_list to Chef-server and run sudo chef-clients then if any remove need update user.run_list
-      str_temp = ""
-      @subject = Subject.find(params[:subject_id])
-      @subject.ku_users.each do |user|
-        str_temp += "ku_id: " + user.ku_id + " - run_list:" + user.run_list.gsub(/\,$/, '')
-        str_temp += " || "
-        user.update_column(:run_list, @kuuser.run_list.gsub("recipe[remove-" + program.program_name + "],", ""))
-        # delete recipe[remove-xxx], from user.run_list
-      end
-
-      render plain: str_temp.inspect
+  def subject_apply# send run_list to Chef-server and run sudo chef-clients then if any remove need update user.run_list
+    str_temp = ""
+    @subject = Subject.find(params[:subject_id])
+    @subject.ku_users.each do |user|# send run_list to Chef-server and run sudo chef-clients 
+      str_temp += "ku_id: " + user.ku_id + " - run_list:" + user.run_list.gsub(/\,$/, '')
+      str_temp += " || "
     end
+    KuUser.where(id: @subject.user_subjects.select("ku_user_id").where(user_enabled: false)).each do |user|
+      @subject.programs.each do |program|
+        # delete recipe[remove-xxx], from user.run_list
+        user.update_column(:run_list, user.run_list.gsub("recipe[remove-" + program.program_name + "],", ""))
+      end
+    end
+    # delete relationship
+    @subject.user_subjects.where(user_enabled: false).destroy_all
+    flash[:success] = str_temp
+    redirect_to subject_user_subjects_path(:subject_id => @subject.id)
 
+  end
+
+  private
     def add_program_to_run_list
       str_temp = ""
-      @kuuser = KuUser.find(params[:ku_user_id])
-      @subject = Subject.find(params[:subject_id])
-      @subject.programs.each do |program|
+      #@kuuser = KuUser.find(params[:ku_user_id])
+      #@subject = Subject.find(params[:subject_id])
+      Program.where(id: @subject.programs_subjects.select("program_id").where(program_enabled: true)).each do |program|
         #str_temp += "ku_id: " + user.ku_id + " add recipe[" + @program.program_name + "] || "
         @kuuser.update_column(:run_list, @kuuser.run_list.to_s + "recipe[" + program.program_name + "],")
       end
@@ -48,10 +74,17 @@ class UserSubjectsController < ApplicationController
 
     def add_remove_program_to_run_list
       str_temp = ""
-      @kuuser = KuUser.find(params[:ku_user_id])
-      @subject = Subject.find(params[:subject_id])
-      @subject.programs.each do |program|
+      #@kuuser = KuUser.find(params[:ku_user_id])
+      #@subject = Subject.find(params[:subject_id])
+      Program.where(id: @subject.programs_subjects.select("program_id").where(program_enabled: true)).each do |program|
         @kuuser.update_column(:run_list, @kuuser.run_list.gsub("recipe[" + program.program_name + "],", "recipe[remove-" + program.program_name + "],"))
+      end
+    end
+
+    def update_program_to_run_list
+      str_temp = ""
+      Program.where(id: @subject.programs_subjects.select("program_id").where(program_enabled: true)).each do |program|
+        @kuuser.update_column(:run_list, @kuuser.run_list.gsub("recipe[remove-" + program.program_name + "],", "recipe[" + program.program_name + "],"))
       end
     end
 end
