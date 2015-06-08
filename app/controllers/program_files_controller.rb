a41230e269ac701ba8c28b60a1d846140369eb76
pakkawat <pakkawat.jong@gmail.com>
2015-06-08 16:27:51 +0700

    test save_file
    test save_file

--- app/controllers/program_files_controller.rb
+++ app/controllers/program_files_controller.rb
@@ -2,7 +2,7 @@ class ProgramFilesController < ApplicationController
   def index
   end
   def show
-  	@program = Program.find(params[:id])
+  	@program = Program.find(params[:program_id])
   	@program_dir = "public/cookbooks/"+@program.program_name+"/"
   	@path = params[:program_files]
   	@current_file = @program_dir+@path
@@ -13,5 +13,16 @@ class ProgramFilesController < ApplicationController
   	else
   	  #file not found
   	end
+  end
+
+  def save_file
+    @program = Program.find(params[:program_id])
+    @program_dir = "public/cookbooks/"+@program.program_name+"/"
+    @path = params[:program_files]
+    @current_file = @program_dir+@path
+    File.open(@current_file, "w+") do |f|
+      f.write(params[:file_data])
+    end
+    redirect_to @program, :notice => "File was saved"
   end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   #match "programs/:program_id/*program_file", :to => "program_files#show", :via => "get"
   resources :programs
   match "programs/:program_id/*program_files", :to => "program_files#show", :via => "get", :constraints => { :program_files => /[^*]+/ }#fix dot in url and allows anything except *
+  match "programs/:program_id/*program_files", :to => "program_files#show", :via => "post", :constraints => { :program_files => /[^*]+/ }
   match '/new_file', to: 'programs#new_file', via: :post
   match '/delete_file', to: 'programs#delete_file', via: :post
   match '/programs/view_file', to: 'programs#view_file', via: :post
