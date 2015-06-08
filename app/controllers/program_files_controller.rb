d7de921e2ea08adfb6e106210fa17c1c770356f4
pakkawat <pakkawat.jong@gmail.com>
2015-06-08 15:52:06 +0700

    test program_files
    test program_files

--- app/controllers/program_files_controller.rb
+++ app/controllers/program_files_controller.rb
@@ -2,6 +2,16 @@ class ProgramFilesController < ApplicationController
   def index
   end
   def show
-    @files = params[:program_files].split("/")
-  end
+  	@program = Program.find(params[:id])
+  	@program_dir = "public/cookbooks/"+@program.program_name+"/"
+  	@path = params[:program_files]
+  	@current_file = @program_dir+@path
+  	if File.directory?(@current_file)
+  	  @all_files = Dir.glob(@current_file+"/*").sort_by{|e| e}
+  	elsif File.file?(@current_file)
+  	  @data = File.read(@current_file)
+  	else
+  	  #file not found
+  	end
+  end	
 end
