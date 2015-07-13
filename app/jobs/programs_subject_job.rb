10159fa2fd6b869d6040ef0cc5198a970541a8f3
pakkawat <pakkawat.jong@gmail.com>
2015-07-13 16:04:06 +0700

    test job
    test job

--- app/jobs/programs_subject_job.rb
+++ app/jobs/programs_subject_job.rb
@@ -1,7 +1,17 @@
 class ProgramsSubjectJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(*args)
-    # Do something later
+  #queue_as :default
+  def initialize(users)
+    @users = users
   end
+
+  def perform
+    # Do something later
+    update_stage('Run command')
+    update_progress_max(@users.count)
+    @users.each do |user|
+      sleep(5)
+      update_progress
+    end
+  end
+  
 end
--- app/jobs/user_subject_job.rb
+++ app/jobs/user_subject_job.rb
@@ -1,7 +1,20 @@
-class UserSubjectJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(*args)
-    # Do something later
+class UserSubjectJob < ProgressJob::Base
+  #queue_as :default
+  def initialize(users)
+    @users = users
   end
+
+  def perform
+    # Do something later
+    update_stage('Run command')
+    update_progress_max(@users.count)
+    @users.each do |user|
+      Dir.chdir("/home/ubuntu/chef-repo") do
+      	system "ruby long.rb"
+      end
+      update_progress
+    end
+    
+  end
+
 end
