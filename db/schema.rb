e36d7a000ee8debdf0aef383e3a99fa2af61e0ed
pakkawat.jong@gmail.com <pakkawat.jong@gmail.com>
2015-07-15 08:15:11 +0000

    add desciption column to delayed_jobs table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711095557) do
+ActiveRecord::Schema.define(version: 20150715081253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150711095557) do
     t.string   "progress_stage"
     t.integer  "progress_current", default: 0
     t.integer  "progress_max",     default: 0
+    t.text     "description"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
