e44d3e22a92f22c2e5ea35f13c0cc6ca5465e427
pakkawat <pakkawat.jong@gmail.com>
2015-09-05 19:04:08 +0700

    test remove_panel_info
    test remove_panel_info

--- app/assets/javascripts/js_dynamic_form/add_remove_field.js
+++ app/assets/javascripts/js_dynamic_form/add_remove_field.js
@@ -2,6 +2,11 @@ function remove_fields(link) {
     $(link).prev("input[type=hidden]").val("1");  
     $(link).closest(".form-group").hide();  
 }  
+
+function remove_panel_info(link) {  
+    $(link).prev("input[type=hidden]").val("1");  
+    $(link).closest(".panel panel-info").hide();  
+}  
   
 function add_fields(link, association, content) {  
     var new_id = new Date().getTime();  
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
 		f.hidden_field(:_destroy) + link_to(name, "#", :onclick => h("remove_fields(this)"), class: "", remote: true)
 	end
 
+  def link_to_remove_panel_info(name, f)  
+    f.hidden_field(:_destroy) + link_to(name, "#", :onclick => h("remove_panel_info(this)"), class: "", remote: true)
+  end
+
 	def link_to_add_fields(name, f, association)  
   		new_object = f.object.class.reflect_on_association(association).klass.new  
   		fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|  
