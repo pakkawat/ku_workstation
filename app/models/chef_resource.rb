dd4aaac4df2cdbdc2c9e7a6286051d917d60f211
pakkawat <pakkawat.jong@gmail.com>
2015-12-24 14:13:15 +0700

    fix bug
    fix bug

--- app/models/chef_resource.rb
+++ app/models/chef_resource.rb
@@ -2,5 +2,5 @@ class ChefResource < ActiveRecord::Base
 	default_scope { order("priority ASC") }
 	belongs_to :program
 	has_many :chef_attributes, :dependent => :destroy
-	accepts_nested_attributes_for :chef_attributes, reject_if: :all_blank, allow_destroy: true
+	accepts_nested_attributes_for :chef_attributes, allow_destroy: true
 end
--- app/models/program.rb
+++ app/models/program.rb
@@ -3,7 +3,7 @@ class Program < ActiveRecord::Base
 	has_many :programs_subjects, dependent: :destroy
 	has_many :subjects, through: :programs_subjects
 	has_many :chef_resources, dependent: :destroy
-        accepts_nested_attributes_for :chef_resources, allow_destroy: true
+    accepts_nested_attributes_for :chef_resources, allow_destroy: true
 	#accepts_nested_attributes_for :chef_resources, reject_if: lambda {|attributes| attributes[:resource_name].blank?}, allow_destroy: true
 	has_many :remove_files, dependent: :destroy
 	has_many :users_programs, dependent: :destroy
