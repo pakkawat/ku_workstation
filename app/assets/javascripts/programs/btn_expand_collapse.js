8c7693a1cf8b1e313ab987ea7447ad1600d8262a
Ubuntu <ubuntu@ip-172-31-19-254.ap-southeast-1.compute.internal>
2016-02-08 10:44:32 +0000

    fix js

--- app/assets/javascripts/programs/btn_expand_collapse.js
+++ app/assets/javascripts/programs/btn_expand_collapse.js
@@ -2,7 +2,8 @@ $(document).off("click", "button.btn.btn-info").on("click", "button.btn.btn-info
 
     $header = $(this);
     //getting the next element
-    $content = $header.prev();
+    //$content = $header.prev();
+$content = $header.parent(".col-md-4.text-center").parent(".row").prev();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
