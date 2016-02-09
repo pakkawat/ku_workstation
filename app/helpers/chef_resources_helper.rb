79a39ff58b19324e416d4fa487a344903dced1f0
pakkawat <pakkawat.jong@gmail.com>
2016-02-09 18:54:06 +0700

    update
    update

--- app/helpers/chef_resources_helper.rb
+++ app/helpers/chef_resources_helper.rb
@@ -1,2 +1,18 @@
 module ChefResourcesHelper
+
+  def resource_type_full_name(resource_type)
+    case resource_type
+    when "Repository"
+      return "Install from repository"
+    when "Deb"
+      return "Install from deb file"
+    when "Source"
+      return "Install from source"
+    when "Download"
+      return "Download file"
+    when "Extract"
+      return "Extract file"
+    end
+  end
+
 end
--- app/helpers/programs_helper.rb
+++ app/helpers/programs_helper.rb
@@ -179,4 +179,20 @@ module ProgramsHelper
     end
   end
 
+  def resource_type_full_name(resource_type)
+    case resource_type
+    when "Repository"
+      return "Install from repository"
+    when "Deb"
+      return "Install from deb file"
+    when "Source"
+      return "Install from source"
+    when "Download"
+      return "Download file"
+    when "Extract"
+      return "Extract file"
+    end
+  end
+
+
 end
