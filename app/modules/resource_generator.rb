bb9881b703f5ebbfb93e09a4ea7c0c0aefe33551
pakkawat <pakkawat.jong@gmail.com>
2016-03-03 20:45:40 +0700

    fix bug and hide copy_file and move_file
    fix bug and hide copy_file and move_file

--- app/modules/resource_generator.rb
+++ app/modules/resource_generator.rb
@@ -87,13 +87,13 @@ require 'uri'
 		source_file = chef_resource.chef_properties.where(:value_type => "source_file").pluck(:value).first
 		program_name = chef_resource.chef_properties.where(:value_type => "program_name").pluck(:value).first
 
-		src_path = File.dirname(source_file)
-		src_file_name = File.basename(source_file)
-		src_paths, src_last_path = get_path(src_path)
+		#src_path = File.dirname(source_file)
+		#src_file_name = File.basename(source_file)
+		#src_paths, src_last_path = get_path(src_path)
 
 		str_code = ""
 		str_code += "dpkg_package \"#{program_name}\" do\n"
-		str_code += "  source '#{src_last_path}\/#{src_file_name}'\n"
+		str_code += "  source '#{source_file}'\n"
 		str_code += "  action :install\n"
 		str_code += "  ignore_failure true\n"
 		str_code += "  notifies :run, 'execute[apt-get-install-f]', :immediately\n"
@@ -120,18 +120,18 @@ require 'uri'
 	def ResourceGenerator.install_from_source(chef_resource)
 		program_name = chef_resource.chef_properties.where(:value_type => "program_name").pluck(:value).first
 		source_file = chef_resource.chef_properties.where(:value_type => "source_file").pluck(:value).first
-		src_paths, src_last_path = get_path(source_file)
+		#src_paths, src_last_path = get_path(source_file)
 		str_code = ""
 		#str_code += "if Dir.entries('#{src_last_path}').size == 2\n" # empty folder (have two links "." and ".." only)
 		str_code += "bash 'install_#{program_name}_from_source' do\n"
 		str_code += "  user 'root'\n"
-		str_code += "  cwd '#{src_last_path}'\n"
+		str_code += "  cwd '#{source_file}'\n"
 		str_code += "  code <<-EOH\n"
 		str_code += "  ./configure\n"
 		str_code += "  make\n"
 		str_code += "  make install\n"
 		str_code += "  EOH\n"
-		str_code += "  not_if \{ Dir.entries('#{des_last_path}').size == 2 \}\n"
+		str_code += "  not_if \{ Dir.entries('#{source_file}').size == 2 \}\n"
 		str_code += "end\n"
 		#str_code += "end\n"
 		str_code += "\n"
@@ -151,7 +151,7 @@ require 'uri'
 		source_file = chef_resource.chef_properties.where(:value_type => "source_file").pluck(:value).first
 
 		src_path = File.dirname(source_file)
-		src_file_name = File.basename(source_file)
+		#src_file_name = File.basename(source_file)
 		src_paths, src_last_path = get_path(src_path)
 
 		str_code = ""
@@ -165,7 +165,7 @@ require 'uri'
 		str_code += "\n"
 
 
-		str_code += "remote_file '#{src_last_path}\/#{src_file_name}' do\n"
+		str_code += "remote_file '#{source_file}' do\n"
 		str_code += "  source '#{url}'\n"
 		str_code += "  mode '0755'\n"
 		str_code += "  action :create\n"
@@ -177,12 +177,12 @@ require 'uri'
 	def ResourceGenerator.delete_download_file(chef_resource)
 		source_file = chef_resource.chef_properties.where(:value_type => "source_file").pluck(:value).first
 
-		src_path = File.dirname(source_file)
-		src_file_name = File.basename(source_file)
-		src_paths, src_last_path = get_path(src_path)
+		#src_path = File.dirname(source_file)
+		#src_file_name = File.basename(source_file)
+		#src_paths, src_last_path = get_path(src_path)
 
 		str_code = ""
-		str_code += "file '#{src_last_path}\/#{src_file_name}' do\n"
+		str_code += "file '#{source_file}' do\n"
 		str_code += "  action :delete\n"
 		str_code += "end\n"
 		str_code += "\n"
@@ -217,7 +217,7 @@ require 'uri'
 		#str_code += "    mkdir -p \"#{extract_to}\"\n"
 		str_code += "    tar xzf #{source_file} \-C #{des_last_path}\n"
 		str_code += "    EOH\n"
-		str_code += "  only_if \{ Dir.entries('#{des_last_path}').size == 2 \}\n"
+		str_code += "  only_if \{ Dir.entries('#{des_last_path}').size == 2 \}\n" # จะแตกไฟล์ก็ต่อเมื่อ folder ปลายทางว่าง
 		str_code += "end\n"
 		str_code += "\n"
 		return str_code
@@ -231,7 +231,7 @@ require 'uri'
 		des_paths, des_last_path = get_path(extract_to)
 
 		str_code = ""
-		str_code += "directory '" + des_last_path + "' do\n"
+		str_code += "directory '#{des_last_path}' do\n"
 		str_code += "  recursive true\n"
 		str_code += "  action :delete\n"
 		str_code += "end\n"
@@ -242,13 +242,13 @@ require 'uri'
 	def ResourceGenerator.config_file(chef_resource, program)
 		source_file = chef_resource.chef_properties.where(:value_type => "config_file").pluck(:value).first
 
-		src_path = File.dirname(source_file)
+		#src_path = File.dirname(source_file)
 		src_file_name = File.basename(source_file)
-		src_paths, src_last_path = get_path(src_path)
+		#src_paths, src_last_path = get_path(src_path)
 
 		str_code = ""
 		if File.exists?("/home/ubuntu/chef-repo/cookbooks/" + program.program_name + "/templates/" + src_file_name + ".erb")
-			str_code += "template '#{src_last_path}\/#{src_file_name}' do\n"
+			str_code += "template '#{source_file}' do\n"
 			str_code += "  source '#{src_file_name}.erb'\n"
 			str_code += "  owner 'root'\n"
 			str_code += "  group 'root'\n"
@@ -256,11 +256,10 @@ require 'uri'
 			str_code += "end\n"
 			str_code += "\n"
 		end
-		str_code += "if ::File.exist?('#{src_last_path}\/#{src_file_name}')\n"
-		str_code += "  file '/var/lib/tomcat7/webapps/ROOT/sharedfile/#{src_file_name}' do\n"
-		str_code += "    content IO.read('#{src_last_path}\/#{src_file_name}')\n"
-		str_code += "    action :create\n"
-		str_code += "  end\n"
+		str_code += "execute 'copy_edit_file' do\n"
+		str_code += "  user 'root'\n"
+		str_code += "  command 'cp #{source_file} /var/lib/tomcat7/webapps/ROOT/sharedfile/#{src_file_name}'\n"
+		str_code += "  only_if { ::File.exist?('#{source_file}') }\n"
 		str_code += "end\n"
 		str_code += "\n"
 		return str_code
@@ -283,9 +282,9 @@ require 'uri'
 		destination_file = chef_resource.chef_properties.where(:value_type => "destination_file").pluck(:value).first
 		str_code = ""
 		if copy_type == "file"
-			src_path = File.dirname(source_file)
-			src_file_name = File.basename(source_file)
-			src_paths, src_last_path = get_path(src_path)
+			#src_path = File.dirname(source_file)
+			#src_file_name = File.basename(source_file)
+			#src_paths, src_last_path = get_path(src_path)
 
 			#des_path = File.dirname(destination_file)
 			#des_file_name = File.basename(destination_file)
@@ -299,11 +298,17 @@ require 'uri'
 			str_code += "  end\n"
 			str_code += "end\n"
 			str_code += "\n"
-			str_code += "if ::File.exist?('#{src_last_path}\/#{src_file_name}')\n"
-			str_code += "  file '#{des_last_path}\/#{src_file_name}' do\n"
-			str_code += "    content IO.read('#{src_last_path}\/#{src_file_name}')\n"
-			str_code += "    action :create\n"
-			str_code += "  end\n"
+			#str_code += "if ::File.exist?('#{src_last_path}\/#{src_file_name}')\n"
+			#str_code += "  file '#{des_last_path}\/#{src_file_name}' do\n"
+			#str_code += "    content IO.read('#{src_last_path}\/#{src_file_name}')\n"
+			#str_code += "    action :create\n"
+			#str_code += "  end\n"
+			#str_code += "end\n"
+			#str_code += "\n"
+			str_code += "execute 'copy_file' do\n"
+			str_code += "  user 'root'\n"
+			str_code += "  command 'cp #{source_file} #{des_last_path}\/#{src_file_name}'\n"
+			str_code += "  only_if { File.exist?('#{source_file}') && !File.exist?('#{des_last_path}\/#{src_file_name}')}\n"
 			str_code += "end\n"
 			str_code += "\n"
 		else
@@ -323,10 +328,15 @@ require 'uri'
 			str_code += "  end\n"
 			str_code += "end\n"
 			str_code += "\n"
-			str_code += "if Dir.entries('#{des_last_path}').size == 2\n" # empty folder (have two links "." and ".." only)
-			str_code += "  execute 'copy_all_file_in_folder' do\n"
-			str_code += "    command 'cp -r #{src_last_path}\/. #{des_last_path}'\n"
-			str_code += "  end\n"
+			#str_code += "if Dir.entries('#{des_last_path}').size == 2\n" # empty folder (have two links "." and ".." only)
+			#str_code += "  execute 'copy_all_file_in_folder' do\n"
+			#str_code += "    command 'cp -r #{src_last_path}\/. #{des_last_path}'\n"
+			#str_code += "  end\n"
+			#str_code += "end\n"
+			#str_code += "\n"
+			str_code += "execute 'copy_all_file_in_folder' do\n"
+			str_code += "  command 'cp -r #{src_last_path}\/. #{des_last_path}'\n"
+			str_code += "  only_if { Dir.entries('#{des_last_path}').size == 2 }\n" # แสดงว่าจะเกิดการ copy folder ได้ก็ต่อเมื่อทำการ copy ไปที่ folder ที่ว่างอยู่เท่านั้น
 			str_code += "end\n"
 			str_code += "\n"
 		end
@@ -445,7 +455,7 @@ require 'uri'
 		str_code += "  end\n"
 		str_code += "end\n"
 		str_code += "\n"
-		if src_file_extname == "" # folder
+		if src_file_extname == "" # folder # ไฟล์บางประเภทก็ไม่มีนามสกุล
 			src_paths, src_last_path = get_path(source_file)
 			str_code += "if Dir.entries('#{des_last_path}').size == 2\n" # empty folder (have two links "." and ".." only)
 			str_code += "  execute 'copy_all_file_in_folder' do\n"
