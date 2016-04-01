b0635734bee3fc39dbc772adddd961504d183edc
pakkawat <pakkawat.jong@gmail.com>
2016-04-01 15:07:53 +0700

    fix error
    fix error

--- app/models/personal_chef_resource.rb
+++ app/models/personal_chef_resource.rb
@@ -1,4 +1,7 @@
 class PersonalChefResource < ActiveRecord::Base
   has_many :personal_program_chefs, dependent: :destroy
   has_many :personal_programs, through: :personal_program_chefs
+  has_many :chef_properties, dependent: :destroy
+  accepts_nested_attributes_for :chef_properties
+  default_scope { order("priority ASC") }
 end
