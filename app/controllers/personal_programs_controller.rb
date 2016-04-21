20059ac57c80773c016044436364bc16955140a8
pakkawat <pakkawat.jong@gmail.com>
2016-04-21 15:50:30 +0700

    update
    update

--- app/controllers/personal_programs_controller.rb
+++ app/controllers/personal_programs_controller.rb
@@ -33,7 +33,7 @@ class PersonalProgramsController < ApplicationController
 
     respond_to do |format|
       if @personal_program.save
-        format.html { redirect_to personal_programs_path, notice: @personal_program.program_name + ' was successfully created.' }
+        format.html { redirect_to personal_programs_path, :flash => { :success => @personal_program.program_name + ' was successfully created.' } }
         format.json { render :show, status: :created, location: @personal_program }
       else
         format.html { render :new }
@@ -47,7 +47,7 @@ class PersonalProgramsController < ApplicationController
   def update
     respond_to do |format|
       if @personal_program.update(personal_program_params)
-        format.html { redirect_to @personal_program, notice: @personal_program.program_name + ' was successfully updated.' }
+        format.html { redirect_to @personal_program, :flash => { :success => @personal_program.program_name + ' was successfully updated.' } }
         format.json { render :show, status: :ok, location: @personal_program }
       else
         format.html { render :edit }
@@ -61,7 +61,7 @@ class PersonalProgramsController < ApplicationController
   def destroy
     @personal_program.destroy
     respond_to do |format|
-      format.html { redirect_to personal_programs_url, notice: 'Personal program was successfully destroyed.' }
+      format.html { redirect_to personal_programs_url, :flash => { :success => 'Personal program was successfully destroyed.' } }
       format.json { head :no_content }
     end
   end
