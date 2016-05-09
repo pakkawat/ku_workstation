e65d78a6b0dc4a3a3ac778a35ca2ba69819c4690
pakkawat <pakkawat.jong@gmail.com>
2016-05-09 19:27:02 +0700

    update
    update

--- config/routes.rb
+++ config/routes.rb
@@ -46,7 +46,7 @@ Rails.application.routes.draw do
   match 'ku_user/:id/create_personal_program', to: 'ku_users#create_personal_program', as: 'ku_user_create_personal_program', :via => "post"
   match 'ku_user/:id/command_job/:job_id', to: 'ku_users#delete_user_job', as: 'delete_user_job', :via => "delete"
   #match 'ku_user/:id/user_personal_program/:user_personal_program_id', to: 'ku_users#delete_personal_program_from_user', as: 'delete_personal_program_from_user', :via => "delete"
-  get 'ku_users/:id/apply_change', to: 'ku_users#apply_change', via: :get
+  match 'ku_users/:id/apply_change', to: 'ku_users#apply_change', as: 'apply_change_ku_user', via: :get
   match 'ku_user/:id/program/:program_id', to: 'ku_users#edit_attribute', as: 'edit_ku_user_attribute', :via => "get"
   match 'ku_user/:id/program/:program_id', to: 'ku_users#update_attribute', as: 'update_ku_user_attribute', :via => "patch"
   match "ku_users/:id/cookbook/*cookbook_paths", :to => "user_cookbook_files#show", :via => "get", :constraints => { :cookbook_paths => /[^*]+/ }
