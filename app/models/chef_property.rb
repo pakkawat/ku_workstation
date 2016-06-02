1c25d0607b60d82c206d03f352c468a6d9d285c3
pakkawat <pakkawat.jong@gmail.com>
2016-06-02 14:09:18 +0700

    update
    update

--- app/models/chef_property.rb
+++ app/models/chef_property.rb
@@ -1,5 +1,15 @@
+class ChefPropertyValidator < ActiveModel::Validator
+  def validate(record)
+    if record.value_type != "configure_optional"
+      unless record.value != ""
+        record.errors[:value] << "Value can't be blank"
+      end
+    end
+  end
+end
+
 class ChefProperty < ActiveRecord::Base
   belongs_to :chef_resource
-  validates :value, :presence => { :message => "Value can't be blank" }
+  validates_with ChefPropertyValidator
   default_scope { order("id ASC") }
 end
