79e2f93f7a2a535aba8d175ed68ea557325fd501
pakkawat <pakkawat.jong@gmail.com>
2016-07-09 18:46:30 +0700

    update
    update

--- app/assets/javascripts/programs/btn_expand_collapse.js
+++ app/assets/javascripts/programs/btn_expand_collapse.js
@@ -1,9 +1,9 @@
-$(document).off("click", "button.btn.btn-info").on("click", "button.btn.btn-info", function(e) {
-
+$(document).off("click", 'a[href="#"]').on("click", 'a[href="#"]', function(e) {
+    e.preventDefault();
     $header = $(this);
     //getting the next element
     //$content = $header.prev();
-$content = $header.parent(".col-md-4.text-center").parent(".row").prev();
+$content = $header.parents(".form-group").next();
     //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
     $content.slideToggle(500, function () {
         //execute this after slideToggle is done
