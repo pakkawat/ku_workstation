b9cf5aabd2efecc4089948d7802c018b9bec4526
pakkawat <pakkawat.jong@gmail.com>
2016-07-10 16:13:34 +0700

    update
    update

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,13 +2,13 @@ class DashboardController < ApplicationController
   def index
     @subjects = Subject.all
     @programs = Program.all
-    @jobs = Delayed::Job.all
+    @jobs = Delayed::Job.all.order("id ASC")
   end
 
   def refresh_jobs
-    @jobs = Delayed::Job.all
-    respond_to do |format|
-        format.js
-    end
+    @jobs = Delayed::Job.all.order("id ASC")
+  	respond_to do |format|
+  		format.js
+  	end
   end
 end
